d806c67c02c861d3b54b2d9b8a488f2d5f8483c7
BuildTools <unconfigured@null.spigotmc.org>
2023-11-14 22:02:27 +0100

    responsive preview page

--- preview.php
+++ preview.php
@@ -18,68 +18,65 @@
                     <p class="alert text-dark w-100" style="font-size: 1.5rem; font-weight: bolder"> Materie </p>
                 </div>
                 <div class="row text-center justify-content-center w-100 mx-0">
-                    <div class="col-10 text-white justify-content-center">
+                    <div class="col-lg-10 col-12 text-white justify-content-center">
                         <div class="row h-100">
-                            <div class="col-4 mx-auto">
+                            <div class="col-lg-4 col-12 mt-1 mx-auto">
                                 <div class="card h-100 bg bg-success text-white">
                                     <div class="card-body">
-                                        <div class="row w-100 justify-content-center text-start">
+                                        <div class="row w-100 justify-content-center text-start mx-0">
                                             <div class="col-3">
                                                 <img src="img/svg/informatica.svg" style="width: 100%" class="img-fluid filter-white"/>
                                             </div>
                                         </div>
-                                        <div class="row w-100 justify-content- center text-start">
+                                        <div class="row w-100 justify-content- center text-start mx-0">
                                             <div class="col-12 mb-2">
                                                 <span class="card-title"> Informatica </span>
                                             </div>
                                             <div class="col-12 text-start justify-content-start">
                                                 <il style="list-style: none">
                                                     <li style="font-weight: bold"> Informatica e Programmazione C/C++/Java</li>
-                                                    <li> Vedi Argomenti </li>
                                                 </il>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-4 mx-auto">
+                            <div class="col-lg-4 col-12 mt-1 mx-auto">
                                 <div class="card h-100 bg bg-success">
                                     <div class="card-body">
-                                        <div class="row w-100 justify-content-center text-start">
+                                        <div class="row w-100 justify-content-center text-start mx-0">
                                             <div class="col-3">
                                                 <img src="img/svg/sistemi.svg" style="width: 100%" class="img-fluid filter-white"/>
                                             </div>
                                         </div>
-                                        <div class="row w-100 justify-content-center text-start">
+                                        <div class="row w-100 justify-content-center text-start mx-0">
                                             <div class="col-12 mb-2">
                                                 <span class="card-title"> Sistemi </span>
                                             </div>
                                             <div class="col-12 text-start justify-content-start">
                                                 <il style="list-style: none">
                                                     <li> Sistemi e Reti Informatiche </li>
-                                                    <li> Vedi Argomenti </li>
                                                 </il>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-4 mx-auto">
+                            <div class="col-lg-4 col-12 mt-1 mx-auto">
                                 <div class="card h-100 bg bg-success">
                                     <div class="card-body">
-                                        <div class="row w-100 justify-content-center text-start">
+                                        <div class="row w-100 justify-content-center text-start mx-0">
                                             <div class="col-3">
                                                 <img src="img/svg/tpsit.svg" style="width: 100%" class="img-fluid filter-white"/>
                                             </div>
                                         </div>
-                                        <div class="row w-100 justify-content-center text-start">
+                                        <div class="row w-100 justify-content-center text-start mx-0">
                                             <div class="col-12 mb-2">
                                                 <span class="card-title"> TPSIT </span>
                                             </div>
                                             <div class="col-12 text-start justify-content-start">
                                                 <il style="list-style: none">
                                                     <li> Tecnologia e Progettazione di Sistemi Informatici e di Telecomunicazione </li>
-                                                    <li> Vedi Argomenti </li>
                                                 </il>
                                             </div>
                                         </div>
@@ -94,14 +91,14 @@
 
         <!-- Second section: Info -->
         <div class="card w-100 bg bg-white py-0 justify-content-center text-center">
-            <div class="card-content col-11 mb-1 text-white mb-2 mx-auto">
+            <div class="card-content col-lg-11 col-12 mb-1 text-white mb-2 mx-auto">
                 <div class="row text-start justify-content-cetner w-100 mx-auto">
                     <p class="alert text-success w-100" style="font-size: 1.5rem; font-weight: bolder"> Argomenti nello Specifico </p>
                 </div>
                 <div class="row w-100 justify-content-center mx-auto my-3">
                     <div class="table-responsive" style="height: 300px">
                         <table class="table-responsive table-hover table-striped bg bg-success">
-                            <thead class="sticky-top">
+                            <thead>
                                 <th scope="col">Materia</th>
                                 <th scope="col">Argomento</th>
                                 <th scope="col">Anno di Riferimento</th>
